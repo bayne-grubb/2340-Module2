af8f91b114ff89e90a5f1bb76f93eb842a83101a
rohanrk <rohanrk2009@gmail.com>
2019-01-24 17:20:34 -0500

    Fix semicolon error

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,11 +31,11 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-        arr = input.toCharArray()
+        arr = input.toCharArray();
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (char) (arr[i] + 1);
         }
-        return new String(arr)
+        return new String(arr);
     }
     
     /**
